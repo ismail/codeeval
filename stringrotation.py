9fcb0e4a39c02683f72e8fa71d768b645fab4313
İsmail Dönmez <ismail@i10z.com>
2016-07-10 21:19:13 +0300

    Simplify stringrotation.py

--- stringrotation.py
+++ stringrotation.py
@@ -10,21 +10,19 @@ with open(sys.argv[1], 'r') as test_cases:
         tmp = word1
         match = False
         
-        if len(words[0]) != len(words[1]):
-            print("False")
-            continue
+        if len(words[0]) == len(words[1]):
+            i = 0
+            l = len(words[0])
+            while i < l:
+                word1.rotate()
 
-        i = 0
-        l = len(words[0])
-        while i < l:
-            word1.rotate()
-
-            if word1 == word2:
-                match = True
-                print("True")
-                break
+                if word1 == word2:
+                    match = True
+                    break
             
-            i += 1
+                i += 1
 
-        if not match:
+        if match:
+            print("True")
+        else:
             print("False")
